130bfb7e2cc117ae0cc0928659179231589e606e
Matthew Orndoff <mattheworndoff@gmail.com>
2018-02-12 11:32:44 -0800

    add basic es6 template literal

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
-var grabCoins = "BTC,ETH,ETC,XRP,LTC,DASH,BCH,XMR,QTUM,ZEC,BTG";
-var url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + grabCoins + '&tsyms=USD';
-var coinNames = ["Bitcoin", "Ethereum", "Ethereum Classic", "Ripple", "Litecoin", "Dash",
+const grabCoins = "BTC,ETH,ETC,XRP,LTC,DASH,BCH,XMR,QTUM,ZEC,BTG";
+const url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + grabCoins + '&tsyms=USD';
+const coinNames = ["Bitcoin", "Ethereum", "Ethereum Classic", "Ripple", "Litecoin", "Dash",
   "Bitcoin Cash", "Monero", "Quantum", "ZCash", "Bitcoin Gold"];
 
-var coinInfo = ["https://en.wikipedia.org/wiki/Bitcoin", "https://en.wikipedia.org/wiki/Ethereum",
+const coinInfo = ["https://en.wikipedia.org/wiki/Bitcoin", "https://en.wikipedia.org/wiki/Ethereum",
   "https://en.wikipedia.org/wiki/Ethereum_Classic", "https://en.wikipedia.org/wiki/Ripple_(payment_protocol)",
   "https://en.wikipedia.org/wiki/Litecoin", "https://en.wikipedia.org/wiki/Dash_(cryptocurrency)",
   "https://en.wikipedia.org/wiki/Bitcoin_Cash", "https://en.wikipedia.org/wiki/Monero_(cryptocurrency)",
@@ -18,12 +17,12 @@ fetch(url)
     }
     res.json().then(function(data) {
       console.log(data);
-      var coins = data.DISPLAY;
-      var main = "";
-      var name, price, coinSymbol, coinDisplay;
-      var x = 0;
+      const coins = data.DISPLAY;
+      let main = "";
+      let name, price, coinSymbol, coinDisplay;
+      let x = 0;
 
-      for (var coin in coins) {
+      for (const coin in coins) {
         if(coins.hasOwnProperty(coin)) {
           name = coin;
           price = coins[coin].USD.PRICE;
@@ -32,18 +31,18 @@ fetch(url)
           if(name.replace(/\s+/, "")  == coinSymbol.replace(/\s+/, "") ) {
             coinSymbol = "";
           } else {
-            coinSymbol = " " + coinSymbol;
+            coinSymbol = "" + coinSymbol;
           }
-          coinDisplay = "<div class='coin'><p class='name'>"
-            + coinNames[x] + " (" + name +  coinSymbol + ") "
-            + "</p>" + "<p>" + price + "</p> "
-            + "<a class='little' href="+ coinInfo[x] + ">" + "What is " + coinNames[x] + "?" + "</a>" + "</div>";
+          coinDisplay = `<div class='card coin'><p class='name'>
+            ${coinNames[x]} (${name} ${coinSymbol})
+            </p><p> ${price} </p>
+            <a class='little' href=${coinInfo[x]}>What is ${coinNames[x]}? </a></div>`
         }
         main += coinDisplay;
         x++;
       }
-      var app = document.getElementsByClassName("app");
-      var footer =  "<div class='little'><p>© 2018</p> <a class='little' href='http://mattheworndoff.com'>" + "Matthew Orndoff" + "</a>" + "</div>";
+      const app = document.getElementsByClassName("app");
+      const footer =  "<div class='little'><p>© 2018</p> <a class='little' href='http://mattheworndoff.com'>" + "Matthew Orndoff" + "</a>" + "</div>";
       main += footer;
       app[0].innerHTML += main;
     });
